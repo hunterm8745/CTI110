029f5f62a8e343d4bc29ff519dd2f5cc0d3fd922
hunterm8745 <hunterm8745@student.faytechcc.edu>
2017-06-06 12:43:31 -0400

    Add files via upload
    Calculate distance traveled using fixed mph and time.

---
+++ M2HW1_DistanceTraveled_Hunterm8745.py
@@ -0,0 +1,14 @@
+# A brief description of the project
+# 6/6/2017
+# CTI-110 M2HW1 - Distance Traveled 
+# Matthew Hunter
+#
+
+# Ask user for speed in mph
+mph = int(input('How fast are you traviling in mph? '))
+
+# Display distances required
+print('Assumming you have no accidents or delays:')
+print('Distance in 6 hours = ', mph * 6,' miles')
+print('Distance in 10 hours = ', mph * 10,' mile')
+print('Distance in 15 hours = ', mph * 15,' miles')
