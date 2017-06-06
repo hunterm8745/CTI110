d27e7513a00fff4a008a88bf66d6a2d64bcfd6e9
hunterm8745 <hunterm8745@student.faytechcc.edu>
2017-06-06 12:42:48 -0400

    Add files via upload
    Predict annual sales based on fixed percentage.

---
+++ M2T1_SalesPrediction_Hunterm8745.py
@@ -0,0 +1,12 @@
+# Predicts annual profits from a company
+# 6/6/2017
+# CTI-110 M2T1 - Sales Prediction
+# Matthew Hunter
+#
+
+percent = 0.23
+annual = 0.0
+
+annual = int(input('Annual Sales = '))
+
+print('Annual Profit = ', annual * percent)
